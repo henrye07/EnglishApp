9e23c9239ba8353b4bf33d38b343b7ea2854d97a
henrye07 <henryeraso7g@gmail.com>
2023-01-24 14:28:19 -0500

    progress in form to exercise

--- lib/B1/ui/screens/present_simple_and_continuous/exercises/topic1.dart
+++ lib/B1/ui/screens/present_simple_and_continuous/exercises/topic1.dart
@@ -16,164 +16,191 @@ class Topic1ExerciseComplete extends StatefulWidget {
 
 class _Topic1ExerciseCompleteState extends State<Topic1ExerciseComplete> {
   String dropdownValue = list.first;
-
+  final _formKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: SingleChildScrollView(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          SizedBox(
-            height: 40,
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                  height: 40,
-                  width: 40,
-                  child: IconButton(
-                    iconSize: 40,
-                    icon: Icon(Icons.keyboard_arrow_left),
-                    color: Colors.blue.shade400,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  )),
-              SizedBox(
-                width: 10,
+        body: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: SingleChildScrollView(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                              height: 40,
+                              width: 40,
+                              child: IconButton(
+                                iconSize: 40,
+                                icon: Icon(Icons.keyboard_arrow_left),
+                                color: Colors.blue.shade400,
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              )),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Flexible(
+                            child: TitleMultiple(
+                              mixed: true,
+                              first_phrase: "The present simple form",
+                              second_phrase: " be",
+                              third_phrase: "",
+                              colors_bold: [
+                                Color(0xff1ABC75),
+                                Color(0xff1ABC75),
+                                Color(0xff1ABC75)
+                              ],
+                              fonts_weight: [
+                                FontWeight.w400,
+                                FontWeight.bold,
+                                FontWeight.w400
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TitleMultiple(
+                        first_phrase: "Topic 1",
+                        color: Colors.black,
+                        font_weight: FontWeight.w500,
+                        text_size: 22,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(219, 234, 234, 234),
+                            boxShadow: [
+                              BoxShadow(
+                                  blurRadius: 5,
+                                  color: Color.fromRGBO(0, 0, 0, 0.4),
+                                  offset: Offset(0, 1))
+                            ]),
+                        child: TextGeneralClass(
+                          text:
+                              "Choose the present simple or present continuous to complete the sentences below.",
+                          font_weight: FontWeight.w300,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      CompleteSentences(
+                        text_1:
+                            "Where do you think the people is looking for find",
+                        text_2: "answer?",
+                        number_exercise: 1,
+                        list_dropdown: ["", "are", "am", "is"],
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      CompleteSentences(
+                        text_1:
+                            "Where do you think the people is looking for find",
+                        text_2: "answer?",
+                        number_exercise: 2,
+                        list_dropdown: ["", "are", "am", "is"],
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      CompleteSentences(
+                        text_1:
+                            "Where do you think the people is looking for find",
+                        text_2: "answer?",
+                        number_exercise: 2,
+                        list_dropdown: ["", "are", "am", "is"],
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      CompleteSentences(
+                        text_1:
+                            "Where do you think the people is looking for find",
+                        text_2: "answer?",
+                        number_exercise: 2,
+                        list_dropdown: ["", "are", "am", "is"],
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      CompleteSentences(
+                        text_1:
+                            "Where do you think the people is looking for find",
+                        text_2: "answer?",
+                        number_exercise: 2,
+                        list_dropdown: ["", "are", "am", "is"],
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      CompleteSentences(
+                        text_1:
+                            "Where do you think the people is looking for find",
+                        text_2: "answer?",
+                        number_exercise: 2,
+                        list_dropdown: ["", "are", "am", "is"],
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      CompleteSentences(
+                        text_1:
+                            "Where do you think the people is looking for find",
+                        text_2: "answer?",
+                        number_exercise: 2,
+                        list_dropdown: ["", "are", "am", "is"],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Validate returns true if the form is valid, or false otherwise.
+                          // if (_formKey.currentState!.validate()) {
+                          //   // If the form is valid, display a snackbar. In the real world,
+                          //   // you'd often call a server or save the information in a database.
+                          //   ScaffoldMessenger.of(context).showSnackBar(
+                          //     const SnackBar(content: Text('Processing Data')),
+                          //   );
+                          // }
+                        },
+                        child: const Text('Submit'),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                    ]),
               ),
-              Flexible(
-                child: TitleMultiple(
-                  mixed: true,
-                  first_phrase: "The present simple form",
-                  second_phrase: " be",
-                  third_phrase: "",
-                  colors_bold: [
-                    Color(0xff1ABC75),
-                    Color(0xff1ABC75),
-                    Color(0xff1ABC75)
-                  ],
-                  fonts_weight: [
-                    FontWeight.w400,
-                    FontWeight.bold,
-                    FontWeight.w400
-                  ],
-                ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TitleMultiple(
-            first_phrase: "Topic 1",
-            color: Colors.black,
-            font_weight: FontWeight.w500,
-            text_size: 22,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color.fromARGB(219, 234, 234, 234),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 5,
-                      color: Color.fromRGBO(0, 0, 0, 0.4),
-                      offset: Offset(0, 1))
-                ]),
-            child: TextGeneralClass(
-              text:
-                  "Choose the present simple or present continuous to complete the sentences below.",
-              font_weight: FontWeight.w300,
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          CompleteSentences(
-            text_1: "Where do you think the people is looking for find",
-            text_2: "answer?",
-            number_exercise: 1,
-            list_dropdown: ["", "are", "am", "is"],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          CompleteSentences(
-            text_1: "Where do you think the people is looking for find",
-            text_2: "answer?",
-            number_exercise: 2,
-            list_dropdown: ["", "are", "am", "is"],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          CompleteSentences(
-            text_1: "Where do you think the people is looking for find",
-            text_2: "answer?",
-            number_exercise: 2,
-            list_dropdown: ["", "are", "am", "is"],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          CompleteSentences(
-            text_1: "Where do you think the people is looking for find",
-            text_2: "answer?",
-            number_exercise: 2,
-            list_dropdown: ["", "are", "am", "is"],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          CompleteSentences(
-            text_1: "Where do you think the people is looking for find",
-            text_2: "answer?",
-            number_exercise: 2,
-            list_dropdown: ["", "are", "am", "is"],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          CompleteSentences(
-            text_1: "Where do you think the people is looking for find",
-            text_2: "answer?",
-            number_exercise: 2,
-            list_dropdown: ["", "are", "am", "is"],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          CompleteSentences(
-            text_1: "Where do you think the people is looking for find",
-            text_2: "answer?",
-            number_exercise: 2,
-            list_dropdown: ["", "are", "am", "is"],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-        ]),
-      ),
-      // Padding(
-      //   padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-      //   child: SizedBox(
-      //     height: 200,
-      //     child: Column(
-      //         // mainAxisAlignment: MainAxisAlignment.start,
-      //         crossAxisAlignment: CrossAxisAlignment.center,
-      //         children: [
-      //           const SizedBox(
-      //             height: 30,
-      //           ),
-      //         ]),
-      //   ),
-      // ),
-      // ]),
-    ));
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+              //   child: SizedBox(
+              //     height: 200,
+              //     child: Column(
+              //         // mainAxisAlignment: MainAxisAlignment.start,
+              //         crossAxisAlignment: CrossAxisAlignment.center,
+              //         children: [
+              //           const SizedBox(
+              //             height: 30,
+              //           ),
+              //         ]),
+              //   ),
+              // ),
+              // ]),
+            )));
   }
 }
--- lib/B1/ui/widgets/dropdown_exercise.dart
+++ lib/B1/ui/widgets/dropdown_exercise.dart
@@ -27,32 +27,45 @@ class _DropDownExerciseState extends State<DropDownExercise> {
     return Container(
       child: Row(
         children: [
-          Container(
-            margin: EdgeInsets.only(top: 15),
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            decoration: BoxDecoration(
-              color: Colors.grey.shade200,
-              borderRadius: BorderRadius.circular(15),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(top: 15, right: 30),
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: DropdownButton(
+                value: dropdownValue,
+                items: widget.list_options
+                    .map<DropdownMenuItem<String>>((String e) =>
+                        DropdownMenuItem(value: e, child: Text(e)))
+                    .toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+                dropdownColor: Colors.grey.shade200,
+                // elevation: 16,
+                style: TextStyle(color: Color(0xff1ABC75)),
+                icon: Icon(Icons.arrow_drop_down),
+                iconSize: 30,
+                underline: Container(
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                          width: 1.0,
+                          style: BorderStyle.solid,
+                          color: Color(0xff1ABC75)),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                  ),
+                ),
+                isExpanded: true,
+              ),
             ),
-            child: DropdownButton(
-              dropdownColor: Colors.grey.shade200,
-              // elevation: 16,
-              style: TextStyle(color: Color(0xff1ABC75)),
-              value: dropdownValue,
-              items: widget.list_options
-                  .map<DropdownMenuItem<String>>(
-                      (String e) => DropdownMenuItem(value: e, child: Text(e)))
-                  .toList(),
-              onChanged: (String? value) {
-                setState(() {
-                  dropdownValue = value!;
-                });
-              },
-              icon: Icon(Icons.arrow_drop_down),
-              iconSize: 30,
-              underline: SizedBox(),
-            ),
-          ),
+          )
         ],
       ),
     );
